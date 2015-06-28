823a7ea6ac1eb9af90ff64efef7e50197dd423a4
Vadim Storozhuk <vadim.storo@gmail.com>
2015-06-27 20:57:54 -0400

    Chapter 6

--- booksrc/sportstoreApp/controllers/productListControllers.js
+++ booksrc/sportstoreApp/controllers/productListControllers.js
@@ -1,11 +1,20 @@
 angular.module("sportsStore")
 	.constant("productListActiveClass", "btn-primary")
-	.controller("productListCtrl", function($scope, $filter, productListActiveClass) {
+	.constant("productListPageCount", 3)
+	.controller("productListCtrl", function($scope, $filter, productListActiveClass, productListPageCount) {
 
 		var selectedCategory = null;
 
+		$scope.selectedPage = 1;
+		$scope.pageSize = productListPageCount;
+
 		$scope.selectCategory = function(newCategory) {
 			selectedCategory = newCategory;
+			$scope.selectedPage = 1;
+		}
+
+		$scope.selectPage = function(newPage) {
+			$scope.selectedPage = newPage;
 		}
 
 		$scope.categoryFilterFn = function(product) {
@@ -15,6 +24,10 @@ angular.module("sportsStore")
 
 		$scope.getCategoryClass = function(category) {
 			return selectedCategory == category ? productListActiveClass : "";
+		}
+
+		$scope.getPageClass = function(page) {
+			return $scope.selectedPage == page ? productListActiveClass : "";
 		}
 
 	});
