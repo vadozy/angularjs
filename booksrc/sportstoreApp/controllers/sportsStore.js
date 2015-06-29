e129db722ace2c9cbd119df3eac537738fc739c1
Vadim Storozhuk <vadim.storo@gmail.com>
2015-06-28 22:14:25 -0400

    Listing 7.2

--- booksrc/sportstoreApp/controllers/sportsStore.js
+++ booksrc/sportstoreApp/controllers/sportsStore.js
@@ -3,7 +3,7 @@ angular.module("sportsStore")
 	.controller("sportsStoreCtrl", function($scope, $http, dataUrl) {
 
 		$scope.data = {};
-		
+
 		$http.get(dataUrl)
 			.success(function(data) {
 				$scope.data.products = data;
