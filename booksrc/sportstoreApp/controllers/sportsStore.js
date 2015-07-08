0e5520dad74875866383663419520ac2b3657fbe
Vadim Storozhuk <vadim.storo@gmail.com>
2015-07-08 17:27:41 -0400

    Listing 8.8

--- booksrc/sportstoreApp/controllers/sportsStore.js
+++ booksrc/sportstoreApp/controllers/sportsStore.js
@@ -1,6 +1,7 @@
 angular.module("sportsStore")
 	.constant("dataUrl", "http://localhost:5500/products")
-	.controller("sportsStoreCtrl", function($scope, $http, dataUrl) {
+	.constant("orderUrl", "http://localhost:5500/orders")
+	.controller("sportsStoreCtrl", function($scope, $http, $location, dataUrl, orderUrl, cart) {
 
 		$scope.data = {};
 
@@ -11,32 +12,19 @@ angular.module("sportsStore")
 			.error(function(error) {
 				$scope.data.error = error;
 			});
-	});
 
-// Below is the original client data controller
-angular.module("sportsStore")
-	.controller("MOCK_sportsStoreCtrl", function($scope) {
-		$scope.data = {
-			products: [{
-				name: "Product #1",
-				description: "A product",
-				category: "Category #1",
-				price: 100
-			}, {
-				name: "Product #2",
-				description: "A product",
-				category: "Category #1",
-				price: 110
-			}, {
-				name: "Product #3",
-				description: "A product",
-				category: "Category #2",
-				price: 210
-			}, {
-				name: "Product #4",
-				description: "A product",
-				category: "Category #3",
-				price: 202
-			}]
-		};
+		$scope.sendOrder = function(shippingDetails) {
+			var order = angular.copy(shippingDetails);
+			order.products = cart.getProducts();
+			$http.post(orderUrl, order)
+				.success(function(data) {
+					$scope.data.orderId = data.id;
+					cart.getProducts().length = 0;
+				})
+				.error(function(error) {
+					$scope.data.orderError = error;
+				}).finally(function() {
+					$location.path("/complete");
+				});
+		}
 	});
